2a0685051439e08fcc33cf6a11bc562f33124d41
Lincher6 <dober-01@yandex.ua>
2020-04-18 13:55:12 +0300

    Updates

---
+++ precache-manifest.191d123e996946e88d67ccd7a5b14e0e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8bf198ee8063d088d8a",
+    "url": "/My-Blog/static/css/main.239df5c4.chunk.css"
+  },
+  {
+    "revision": "a8bf198ee8063d088d8a",
+    "url": "/My-Blog/static/js/main.a8bf198e.chunk.js"
+  },
+  {
+    "revision": "c4938546e53a7e750022",
+    "url": "/My-Blog/static/js/1.c4938546.chunk.js"
+  },
+  {
+    "revision": "b45abeea53641bcfbe15",
+    "url": "/My-Blog/static/js/runtime~main.b45abeea.js"
+  },
+  {
+    "revision": "2da1ff8dc6a77491ac7b16d3fd7431f3",
+    "url": "/My-Blog/static/media/empty-avatar.2da1ff8d.png"
+  },
+  {
+    "revision": "8fdeecf49647c15f6d33f1a19781a2b2",
+    "url": "/My-Blog/static/media/umbrella-logo.8fdeecf4.png"
+  },
+  {
+    "revision": "7891e37e943f99ff927f2f6d49b48c13",
+    "url": "/My-Blog/static/media/appBackground_2.7891e37e.jpg"
+  },
+  {
+    "revision": "6b76fde3b623d8b4a2976abba1c9305e",
+    "url": "/My-Blog/static/media/profileIcon.6b76fde3.png"
+  },
+  {
+    "revision": "6c93f56222ef50421df40b9cbedf9a6c",
+    "url": "/My-Blog/static/media/dialogIcon.6c93f562.png"
+  },
+  {
+    "revision": "7f14165c85fa455d739406c6db74ebd5",
+    "url": "/My-Blog/static/media/usersIcon.7f14165c.png"
+  },
+  {
+    "revision": "d7936c3ed2ad884793fb589acc26059e",
+    "url": "/My-Blog/static/media/gearIcon.d7936c3e.png"
+  },
+  {
+    "revision": "b1c58ee19d1e2c38b29ae86f3174f2e4",
+    "url": "/My-Blog/static/media/instagram.b1c58ee1.svg"
+  },
+  {
+    "revision": "5d9ecf94770b65039cea7b256be82857",
+    "url": "/My-Blog/static/media/vk.5d9ecf94.png"
+  },
+  {
+    "revision": "a888fec0a2859e59674a89bc7722e062",
+    "url": "/My-Blog/static/media/telegram.a888fec0.svg"
+  },
+  {
+    "revision": "a04938a02f163104931918076f820c8d",
+    "url": "/My-Blog/static/media/youtube.a04938a0.png"
+  },
+  {
+    "revision": "ddf5f03e1a924d954196edd5d8f5e2c3",
+    "url": "/My-Blog/static/media/BlobSound.ddf5f03e.ogg"
+  },
+  {
+    "revision": "41ecc497b2d06f57b7d9286bf1328670",
+    "url": "/My-Blog/static/media/log-in.41ecc497.png"
+  },
+  {
+    "revision": "8f14a4e6bb27a65c236a22c075e5d226",
+    "url": "/My-Blog/static/media/register.8f14a4e6.png"
+  },
+  {
+    "revision": "81012bb846fe2790170470216b62bef7",
+    "url": "/My-Blog/static/media/plus.81012bb8.png"
+  },
+  {
+    "revision": "cef41e2541cdc322139c5ee5c32e0b21",
+    "url": "/My-Blog/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/My-Blog/precache-manifest.191d123e996946e88d67ccd7a5b14e0e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/My-Blog/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
